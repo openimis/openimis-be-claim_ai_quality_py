cd7e362bc834695a8ca59d33589f390ee33ed7b8
Damian Borowiecki <dborowiecki@soldevelo.com>
2021-02-04 10:56:30 +0100

    OAI-30: Improved report

--- claim_ai_quality/apps.py
+++ claim_ai_quality/apps.py
@@ -5,10 +5,14 @@ MODULE_NAME = 'claim_ai_quality'
 DEFAULT_CONFIG = {
     "claim_ai_url": "ws://localhost:8000/claim_ai/ws/Claim/process/",
     "event_based_activation": False,
-    "bundle_size": 100,
+    "bundle_size": 3,
     "zip_bundle": False,
     "connection_timeout": 5,
-    "authentication_token": ''
+    "authentication_token": 'token',
+    "accepted_category_code": 0,
+    "rejected_category_code": 1,
+    "reason_rejected_by_ai_code": -2,
+    "date_format": '%Y-%m-%d'
 }
 
 
@@ -21,6 +25,10 @@ class ClaimAiQualityConfig(AppConfig):
     zip_bundle = DEFAULT_CONFIG["zip_bundle"]
     connection_timeout = DEFAULT_CONFIG["connection_timeout"]
     authentication_token = DEFAULT_CONFIG["authentication_token"]
+    rejected_category_code = DEFAULT_CONFIG["rejected_category_code"]
+    accepted_category_code = DEFAULT_CONFIG["accepted_category_code"]
+    reason_rejected_by_ai_code = DEFAULT_CONFIG["reason_rejected_by_ai_code"]
+    date_format = DEFAULT_CONFIG['date_format']
 
     def _configure_perms(self, cfg):
         for config, config_value in cfg.items():
--- claim_ai_quality/views.py
+++ claim_ai_quality/views.py
@@ -1,9 +1,17 @@
+from collections import defaultdict, namedtuple
+from functools import lru_cache
+from datetime import datetime
+
 from claim import ClaimConfig
-from claim.models import ClaimDetail, ClaimItem, ClaimService
+from claim.models import ClaimDetail, ClaimItem, ClaimService, Claim
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import render
 from django.utils.translation import gettext as _
-from .report import template
+from typing import Union
+from claim_ai_quality.apps import ClaimAiQualityConfig
+
+from .report_template import template
+from .report import MisclassificationReportBuilder
 # Create your views here.
 from report.services import ReportService
 
@@ -14,44 +22,5 @@ def miscategorisation_report(request):
     report_service = ReportService(request.user)
     report_data_service = AiQualityReportService(request.user)
     data = report_data_service.fetch(request.GET)
-
-    all_valuated = 0
-    result = {
-        'true_positive': 0,
-        'true_negative': 0,
-        'false_positive': 0,
-        'false_negative': 0,
-    }
-    errors = []
-    for provided in data:
-        statuses = provided.status, provided.json_ext['claim_ai_quality']['ai_result']
-        if statuses == (ClaimDetail.STATUS_PASSED, '1'):
-            result['true_positive'] += 1
-        elif statuses == (ClaimDetail.STATUS_PASSED, '2'):
-            result['false_negative'] += 1
-        elif statuses == (ClaimDetail.STATUS_REJECTED, '1'):
-            result['false_positive'] += 1
-        elif statuses == (ClaimDetail.STATUS_REJECTED, '2'):
-            result['true_negative'] += 1
-        else:
-            errors.append((
-                provided.claim.code,
-                provided.item.code if isinstance(provided, ClaimItem) else provided.service.code,
-                statuses
-            ))
-
-        all_valuated += 1
-
-    valid_evaluations = (result['true_positive'] + result['true_negative'])
-    data = {
-        'all': str(all_valuated),
-        'accuracy': str(valid_evaluations / all_valuated) if all_valuated != 0 else 'N/A',
-        **{str(k): str(v) for k, v in result.items()},
-        **{F"{key}_percentage": F"{(value/all_valuated)*100:.2f}%" for key, value in result.items()
-           if all_valuated}
-    }
-
-    if errors:
-        data['errors'] = str(errors)
-
-    return report_service.process('claim_ai_misclassification', data, template)
+    report_builder = MisclassificationReportBuilder()
+    return report_service.process('claim_ai_misclassification', report_builder.build_report_data(data), template)
