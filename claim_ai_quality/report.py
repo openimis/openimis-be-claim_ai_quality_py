34e965df0790dc5b59335e1585b15c8ece043770
Damian Borowiecki <dborowiecki@soldevelo.com>
2021-02-05 10:29:34 +0100

    OAI-30: Added missing fields to report

--- claim_ai_quality/report.py
+++ claim_ai_quality/report.py
@@ -1,6 +1,7 @@
 from collections import namedtuple, defaultdict
 from datetime import datetime
 from functools import lru_cache
+from core.models import User, InteractiveUser
 
 from claim.models import Claim, ClaimDetail, ClaimItem, ClaimService
 
@@ -11,7 +12,7 @@ class MisclassificationReportBuilder:
     ClaimKey = namedtuple('ClaimErrorKey', [
         'claim_id', 'claim_status', 'claim_claimed', 'claim_approved',
         'hf', 'hf_code', 'hf_name', 'location_name', 'location_code',
-        'date', 'visit_from', 'visit_to', 'insuree_number', 'insuree_name', 'claim_admin'
+        'date', 'visit_from', 'visit_to', 'insuree_number', 'insuree_name', 'claim_admin', 'claim_reviewer'
     ])
 
     def __init__(self):
@@ -44,6 +45,10 @@ class MisclassificationReportBuilder:
             'all': str(self.total_items_and_services),
             'total_claims': str(self.total_claims),
             'accuracy': self._percentage_repr(self.accuracy),
+            'true_positive': str(self.true_positive) or '0',
+            'true_negative': str(self.true_negative) or '0',
+            'false_positive': str(self.false_positive) or '0',
+            'false_negative': str(self.false_negative) or '0',
             'true_positive_percentage': self._percentage_repr(self.true_positive/self.total_items_and_services),
             'true_negative_percentage': self._percentage_repr(self.true_negative/self.total_items_and_services),
             'false_positive_percentage': self._percentage_repr(self.false_positive/self.total_items_and_services),
@@ -58,6 +63,7 @@ class MisclassificationReportBuilder:
     def missclassified_record_key(self, claim: Claim):
         insuree = claim.insuree
         admin = claim.admin
+        reviewer = self.__get_audit_user(claim.audit_user_id_review)
         return self.ClaimKey(**{
             'claim_id': claim.code,
             'claim_status': str(claim.status),
@@ -74,6 +80,7 @@ class MisclassificationReportBuilder:
             'insuree_number': insuree.chf_id,
             'insuree_name': F"{insuree.other_names} {insuree.last_name}",
             'claim_admin': F"{admin.other_names} {admin.last_name}",
+            'claim_reviewer': F"{reviewer.other_names} {reviewer.last_name}" if reviewer else "-"
         })
 
     def clear_report_data(self):
@@ -105,19 +112,21 @@ class MisclassificationReportBuilder:
 
     def false_evaluation_item(self, provided: ClaimItem):
         item = provided.item
-        return self.__provided_entry(provided, item.code)
+        return self.__provided_entry(provided, item.code, 'I')
 
     def false_evaluation_service(self, provided: ClaimService):
         service = provided.service
-        return self.__provided_entry(provided, service.code)
+        return self.__provided_entry(provided, service.code, 'S')
 
     def false_evaluation_entry(self, key, provided):
         return self.false_evaluation_item(provided) if isinstance(provided, ClaimItem) \
             else self.false_evaluation_service(provided)
 
-    def __provided_entry(self, provided, code):
+    def __provided_entry(self, provided, code, type):
         return {
             'code': code,
+            'provision_type': type,
+            'item_status': str(provided.status),
             'quantity': provided.qty_provided,
             'quantity_approved': provided.qty_approved,
             'price': provided.price_asked,
@@ -134,3 +143,10 @@ class MisclassificationReportBuilder:
                 item = {**k._asdict(), **next_item}
                 list_of_items.append(item)
         return list_of_items
+
+    @lru_cache(None)
+    def __get_audit_user(self, audit_user_id):
+        if audit_user_id:
+            return InteractiveUser.objects.get(id=audit_user_id)
+        else:
+            return None
